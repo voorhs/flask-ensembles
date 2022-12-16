2351f9ae39a8c4d606d21a4b28a3120e61a20635
voorhs <ilya_alekseev_2016@list.ru>
2022-12-16 06:27:57 +0300

    added convergence history to ensembles.py

--- src/ensembles.py
+++ src/ensembles.py
@@ -7,7 +7,7 @@ bagging_const = 1 - 1 / e
 
 class RandomForestMSE:
     def __init__(
-        self, n_estimators, max_depth=None, feature_subsample_size=None,
+        self, n_estimators, max_depth=None, subspace_size=None,
         **trees_parameters
     ):
         """
@@ -17,11 +17,11 @@ class RandomForestMSE:
         max_depth : int
             The maximum depth of the tree. If None then there is no limits.
 
-        feature_subsample_size : float
+        subspace_size : float
             The size of feature set for each tree. If None then use one-third of all features.
         """
         self.n_estimators = n_estimators
-        self.feature_subsample_size = feature_subsample_size
+        self.subspace_size = subspace_size
         self.forest = []
         for i in range(n_estimators):
             self.forest.append(
@@ -41,15 +41,24 @@ class RandomForestMSE:
         y_val : numpy ndarray
             Array of size n_val_objects
         """
-        if self.feature_subsample_size is None:
-            self.feature_subsample_size = 1 / 3
+        # convergence history
+        trace = X_val is not None and y_val is not None
+        conv = {'train': [], 'val': []}
+
+        if self.subspace_size is None:
+            self.subspace_size = 1 / 3
         self.subspace_size = int(
-            np.ceil(X.shape[1] * self.feature_subsample_size))
+            np.ceil(X.shape[1] * self.subspace_size))
         subsample_size = int(np.ceil(X.shape[0] * bagging_const))
 
         self.subspaces = np.empty(
             (len(self.forest), self.subspace_size), dtype='int')
         rng = np.random.default_rng()
+
+        predictions = np.zeros(X.shape[0])
+        if trace:
+            val_preds = np.zeros(X_val.shape[0])
+
         for i in range(self.n_estimators):
             self.subspaces[i] = rng.choice(X.shape[1],
                                            size=self.subspace_size,
@@ -60,6 +69,18 @@ class RandomForestMSE:
                                    replace=True,
                                    shuffle=False).reshape(-1, 1)
             self.forest[i].fit(X[subsample, self.subspaces[i]], y[subsample])
+            
+            predictions += self.forest[i].predict(X[:, self.subspaces[i]])
+            conv['train'].append(
+                np.sqrt(np.mean((predictions / (i + 1) - y) ** 2)))
+            
+            if trace:
+                val_preds += self.forest[i].predict(
+                    X_val[:, self.subspaces[i]])
+                conv['val'].append(
+                    np.sqrt(np.mean((val_preds / (i + 1) - y_val) ** 2)))
+        
+        return conv
 
     def predict(self, X):
         """
@@ -80,7 +101,7 @@ class RandomForestMSE:
 
 class GradientBoostingMSE:
     def __init__(
-        self, n_estimators, learning_rate=0.1, max_depth=5, feature_subsample_size=None,
+        self, n_estimators, learning_rate=0.1, max_depth=5, subspace_size=None,
         **trees_parameters
     ):
         """
@@ -93,11 +114,11 @@ class GradientBoostingMSE:
         max_depth : int
             The maximum depth of the tree. If None then there is no limits.
 
-        feature_subsample_size : float
+        subspace_size : float
             The size of feature set for each tree. If None then use one-third of all features.
         """
         self.n_estimators = n_estimators
-        self.feature_subsample_size = feature_subsample_size
+        self.subspace_size = subspace_size
         self.lr = learning_rate
         self.forest = []
         for i in range(n_estimators):
@@ -112,12 +133,16 @@ class GradientBoostingMSE:
         y : numpy ndarray
             Array of size n_objects
         """
-        if self.feature_subsample_size is None:
-            self.feature_subsample_size = 1 / 3
-        self.subspace_size = int(
-            np.ceil(X.shape[1] * self.feature_subsample_size))
+        # convergence history
+        trace = X_val is not None and y_val is not None
+        conv = {'train': [], 'val': []}
 
         # initializing
+        if self.subspace_size is None:
+            self.subspace_size = 1 / 3
+        self.subspace_size = int(
+            np.ceil(X.shape[1] * self.subspace_size))
+
         self.subspaces = np.empty(
             (self.n_estimators, self.subspace_size), dtype='int')
         self.weights = [1]
@@ -129,7 +154,9 @@ class GradientBoostingMSE:
                 X.shape[1], size=self.subspace_size, replace=False, shuffle=False)
 
         shift = y.copy()
-        predictions = 0
+        predictions = np.zeros(X.shape[0])
+        if trace:
+            val_preds = np.zeros(X_val.shape[0])
         for i in range(self.n_estimators):
             # fit new tree to approximate shift
             self.forest[i].fit(X[:, self.subspaces[i]], shift)
@@ -145,8 +172,15 @@ class GradientBoostingMSE:
             predictions += self.lr * self.weights[i] * predicted
 
             shift = y - predictions
+            conv['train'].append(np.sqrt(np.mean(shift ** 2)))
 
-        return self
+            if trace:
+                val_preds += self.lr * \
+                    self.weights[i] * \
+                    self.forest[i].predict(X_val[:, self.subspaces[i]])
+                conv['val'].append(np.sqrt(np.mean((y_val - val_preds) ** 2)))
+        
+        return conv
 
     def predict(self, X):
         """
